9b5f2fe5e6d022a25276b599ee27ced4518e3fe0
Daniel Berry <dberry@carnegietechnologies.com>
2018-04-06 21:01:45 -0500

    Updates testing tools
    * Adds utilities/functions with create and make helpers
    * Adds a signIn helper to TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,31 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Setup the test environment.
+     *
+     * @return void
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->withoutExceptionHandling();
+    }
+
+    /**
+     * Helper method for signing in an active user.
+     *
+     * @param null $user
+     * @return \Tests\TestCase
+     */
+    protected function signIn($user = null)
+    {
+        $user = $user ? : create(User::class);
+
+        $this->actingAs($user);
+
+        return $this;
+    }
 }
